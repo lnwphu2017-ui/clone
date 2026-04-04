950ad4bedeb29e06a913d3b113a9dc2a447d6777
korwlik23 <korwlik23@gmail.com>
2026-04-04 21:39:50 +0700

    fix33

--- api/index.py
+++ api/index.py
@@ -27,7 +27,7 @@ def on_startup():
 AVAILABLE_MODELS = [
 
     {"id": "google/gemini-2.0-flash-001", "name": "Gemini 2.0 Flash"},
-    {"id": "google/gemini-2.0-flash-lite-preview-02-05:free", "name": "Gemini 2.0 Flash Lite "}
+    {"id": "google/gemini-2.0-flash-lite-preview-02-05:free", "name": "Gemini 2.0 Flash Lite "},
     {"id": "openai/gpt-oss-120b:free", "name": "GPT OSS 120B "},
     {"id": "deepseek/deepseek-r1:free", "name": "DeepSeek R1 "},
     {"id": "qwen/qwen3.6-plus:free", "name": "Qwen 3.6 Plus "},
